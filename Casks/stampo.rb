93fc2a5ab6be3a44b6d44c79080977076f3e0dc8
hex000 <chernov.email@yandex.com>
2026-08-26 22:46:16 +0500

    Update Homebrew cask to 0.9.0

--- Casks/stampo.rb
+++ Casks/stampo.rb
@@ -1,6 +1,6 @@
 cask "stampo" do
-  version "0.8.2"
-  sha256 "1dae51806f07c665d96b55447c566f50d889a87f944d94a891b17e2f9ef731d4"
+  version "0.9.0"
+  sha256 "f4d2ed2302f81a83d1999cd14f57e96d46e40f4b10bf295ecc39bb3a44295c3b"
 
   url "https://github.com/git-webuser/Stampo/releases/download/#{version}/Stampo-#{version}.dmg"
   name "Stampo"
